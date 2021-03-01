290ad221b77fa14a73041e8c4427707556d0642e
Lesley Kimutai <leskimuti@gmail.com>
2021-03-01 16:54:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members:[{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      }
+    }]
+
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us1.api.mailchimp.com/3.0/lists/e1b2c738d3";
+
+  const options = {
+    method: "POST",
+    auth: "leskim:2783a77d9eb34a9c7bb26f4b6cb8161d-us1",
+  };
+
+  const request = https.request(url, options, function(response){
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    }else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+    console.log(JSON.parse(data));
+    });
+  });
+
+  // request.write('<head><meta charset="utf-8"></head>');
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
+});
+
+// API Key
+// 2783a77d9eb34a9c7bb26f4b6cb8161d-us1
+
+// List Id
+// e1b2c738d3

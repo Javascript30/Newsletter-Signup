b4e6bde41ee7fdd84993787e93d8423d663041a1
Lesley <leskimuti@gmail.com>
2023-02-28 15:21:26 +0300

    Added new key and some changes

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 // jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-const ejs = require('ejs');
+const ejs = require("ejs");
 
+require("dotenv").config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const year = new Date().getFullYear();
 
-app.get("/", function(req, res){
-  res.render("signup", {year: year});
+app.get("/", function(req, res) {
+  res.render("signup", { year: year });
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
 
   const data = {
-    members:[{
-      email_address: email,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
-      }
-    }]
-
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
   };
 
   const jsonData = JSON.stringify(data);
@@ -43,38 +47,36 @@ app.post("/", function(req, res){
 
   const options = {
     method: "POST",
-    auth: "leskim:2783a77d9eb34a9c7bb26f4b6cb8161d-us1",
+    auth: `1eskim:${process.env.keymailchimp}`,
   };
 
-  const request = https.request(url, options, function(response){
-
+  const request = https.request(url, options, function(response) {
+    console.log(response.statusCode);
     if (response.statusCode === 200) {
       res.render("success");
-    }else{
+    } else {
       res.render("failure");
     }
 
-    response.on("data", function(data){
-    console.log(JSON.parse(data));
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
     });
   });
-
   // request.write('<head><meta charset="utf-8"></head>');
   request.write(jsonData);
   request.end();
-
 });
 
-app.post("/failure", function(req, res){
+app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000");
 });
 
 // API Key
-// 2783a77d9eb34a9c7bb26f4b6cb8161d-us1
+// 2783a77d9eb34a9c7bb26f4b6c
 
 // List Id
 // e1b2c738d3
